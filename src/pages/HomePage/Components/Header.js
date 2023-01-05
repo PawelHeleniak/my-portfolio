b31d59c493fcd6f0b795cdd48f3d12d26da838aa
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-01-05 09:52:52 +0100

    Added default header

---
+++ src/pages/HomePage/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="wrapper header">
+        <div className="primaryDescription">
+          <h1>Hello,</h1>
+          <h1>I`m Paweł</h1>
+          <h2>Front end developer</h2>
+        </div>
+        <div className="secoundaryDescription">
+          <p>I'm learning to create websites in modern technologies, the main technologies I used: JavaScript, React, Css/Scss</p>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Header } from './Components/Header'
 
 export const HomePage = () => {
   return (
-    <div><p>TEST</p></div>
+    <Header />
   )
 }
